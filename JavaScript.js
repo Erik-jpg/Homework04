309f5ce2fad4cd62c8f11c9f08710313d6f318a1
Erik Stone <spologas@gmail.com>
2021-05-24 16:54:05 -0700

    first commit and psuedocoded criteria

---
+++ JavaScript.js
@@ -0,0 +1,13 @@
+//Criteria:
+//When I click the start button, a timer starts 
+    //add button and eventListener to start timer
+//When I answer a question, then I am presented with another question
+   //User Interactions prompt the next question
+//If an answer is incorrect, time is subtracted from the timer
+     //compare an array (answers) and store the data (score)
+//The quiz ends if all the questions are answered or the timer ends
+    //after x amount answers (this could end the timer) or time runs out, the application ends
+// people who have made it to the end of the quiz can leave their initials
+    //store and compare answers given and an array of correct answers and then place in a table of possible spots and allow the user to leave initials
+
+    
