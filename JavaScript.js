f61b22b870f4684ce105f1253f0fc51231fe5db2
Erik Stone <spologas@gmail.com>
2021-05-24 22:38:20 -0700

    pseudocoding with Maryus

--- JavaScript.js
+++ JavaScript.js
@@ -10,4 +10,34 @@
 // people who have made it to the end of the quiz can leave their initials
     //store and compare answers given and an array of correct answers and then place in a table of possible spots and allow the user to leave initials
 
-    
+    //start with one question and then go on to the next
+    //I think maybe switching between pages for questions?
+    //set onclick event with if correct or incorrect answers
+    //maybe a boolean? correct = true, incorrect = false?
+    //set id for correct and a different one for wrong
+
+    //example: const rightAnswer = document.querySelector('#right-answer');
+    //rightAnswer.addEventListener('click')
+
+    //object might be easier than an array
+
+    //const questionArray = [ {
+    //question: 'What is the best food?'
+    //option: ['tacos', 'sushi', 'borsh', 'hamburgers']
+    //answer: 0
+    //}
+    //];
+
+    //<section>
+    //<h1 id ='question'></h1>
+    //<ul id='options'>
+    //</ul>
+
+//function renderQuestion (question) {
+//     const questionEl = document.querySelector('#question');
+//     questionEl.textContent= question.text;
+
+//     question.options.array.forEach(handleOption
+//         const liEl = document.create
+//     })
+// }
