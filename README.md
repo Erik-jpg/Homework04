17a39ffb1c87d70f9a30c87130fc109e81ae313d
Erik Stone <spologas@gmail.com>
2021-05-29 11:19:45 -0700

    made pages, then used code that study group One and I wrote

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Homework04
+
 Web APIs Code Quiz Challenge
+This is a quiz about JavaScript facts for U of O's Full Stack Web Developer, Part Time.
+Using Two Html pages, One Css page and two JavaScript pages, I have made a timed quiz that subtracts points for incorrect answers and when the user clicks the submit button, they are then prompted by the next question of the quiz have fun and leave your initials on the score board!
--- score.js
+++ score.js
@@ -20,26 +20,35 @@
     //rightAnswer.addEventListener('click')
 
     //object might be easier than an array
-
-    //const questionArray = [ {
-    //question: 'What is the best food?'
-    //option: ['tacos', 'sushi', 'borsh', 'hamburgers']
-    //answer: 0
-    //}
-    //];
-
-    //<section>
-    //<h1 id ='question'></h1>
-    //<ul id='options'>
-    //</ul>
-
-//function renderQuestion (question) {
-//     const questionEl = document.querySelector('#question');
-//     questionEl.textContent= question.text;
-
-//     question.options.array.forEach(handleOption
-//         const liEl = document.create
-//     })
-// }
-
-// 
+    var storedScores = JSON.parse(localStorage.getItem("userData"));
+    var highScoresArea = document.querySelector("#highScoresList");
+    var backBtn = document.querySelector("#backButton");
+    var clearBtn = document.querySelector("#clearScores");
+    
+    
+    function displayScores() {
+        if (storedScores !== null) {
+            var scoreList = document.createElement("ol");
+            scoreList.className = "scoreListClass";
+            for (var i = 0; i < storedScores.length; i++) {
+                var initials = storedScores[i].inits;
+                var scores = storedScores[i].userScore
+                var scoreEntry = document.createElement("li");
+                scoreEntry.innerHTML = initials + " - " + scores;
+                scoreList.appendChild(scoreEntry);
+            }
+            highScoresArea.appendChild(scoreList);
+        }
+    };
+    
+    displayScores();
+    
+    backBtn.addEventListener("click", function () {
+        location.href = "index.html";
+    });
+    
+    clearBtn.addEventListener("click", function () {
+        highScoresArea.innerHTML = "";
+        window.localStorage.clear();
+    
+    });
