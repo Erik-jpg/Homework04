ff4107a54247a24e11090d10f3a2494644f90894
Erik Stone <spologas@gmail.com>
2021-06-02 17:48:30 -0700

    error, shuffled array is undefinded

--- score.js
+++ score.js
@@ -19,36 +19,36 @@
     //example: const rightAnswer = document.querySelector('#right-answer');
     //rightAnswer.addEventListener('click')
 
-    //object might be easier than an array
-    var storedScores = JSON.parse(localStorage.getItem("userData"));
-    var highScoresArea = document.querySelector("#highScoresList");
-    var backBtn = document.querySelector("#backButton");
-    var clearBtn = document.querySelector("#clearScores");
+//     //object might be easier than an array
+//     var storedScores = JSON.parse(localStorage.getItem("userData"));
+//     var highScoresArea = document.querySelector("#highScoresList");
+//     var backBtn = document.querySelector("#backButton");
+//     var clearBtn = document.querySelector("#clearScores");
     
     
-    function displayScores() {
-        if (storedScores !== null) {
-            var scoreList = document.createElement("ol");
-            scoreList.className = "scoreListClass";
-            for (var i = 0; i < storedScores.length; i++) {
-                var initials = storedScores[i].inits;
-                var scores = storedScores[i].userScore
-                var scoreEntry = document.createElement("li");
-                scoreEntry.innerHTML = initials + " - " + scores;
-                scoreList.appendChild(scoreEntry);
-            }
-            highScoresArea.appendChild(scoreList);
-        }
-    };
+//     function displayScores() {
+//         if (storedScores !== null) {
+//             var scoreList = document.createElement("ol");
+//             scoreList.className = "scoreListClass";
+//             for (var i = 0; i < storedScores.length; i++) {
+//                 var initials = storedScores[i].inits;
+//                 var scores = storedScores[i].userScore
+//                 var scoreEntry = document.createElement("li");
+//                 scoreEntry.innerHTML = initials + " - " + scores;
+//                 scoreList.appendChild(scoreEntry);
+//             }
+//             highScoresArea.appendChild(scoreList);
+//         }
+//     };
     
-    displayScores();
+//     displayScores();
     
-    backBtn.addEventListener("click", function () {
-        location.href = "index.html";
-    });
+//     backBtn.addEventListener("click", function () {
+//         location.href = "index.html";
+//     });
     
-    clearBtn.addEventListener("click", function () {
-        highScoresArea.innerHTML = "";
-        window.localStorage.clear();
+//     clearBtn.addEventListener("click", function () {
+//         highScoresArea.innerHTML = "";
+//         window.localStorage.clear();
     
-    });
+//     });
